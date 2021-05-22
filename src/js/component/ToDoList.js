c19320ad4540b9e897960d298e568e7f09dd09c4
Kty <katiuska.rodriguez5@gmail.com>
2021-05-22 10:35:31 +0000

    todoListApi

--- src/js/component/ToDoList.js
+++ src/js/component/ToDoList.js
@@ -7,7 +7,36 @@ const ToDoList = () => {
 		const newTodos = todos.concat({ title: task, id: todos.length + 1 });
 		setTodos(newTodos);
 	};
+	fetch("https://assets.breatheco.de/apis/fake/todos/user/mytodoList")
+		.then(response => response.json())
+        .then(responseJSON => setTodos(responseJSON));
+        
 
+    useEffect(() => {
+        fetch("http://assets.breatheco.de/apis/fake/todos/user/mytodoList", {
+		method: "PUT",
+		body: JSON.stringify(todos),
+		headers: {
+			"Content-Type": "application/json"
+		}
+	})
+		.then(resp => {
+			console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
+			console.log(resp.status); // el código de estado = 200 o código = 400 etc.
+			console.log(resp.text()); // Intentará devolver el resultado exacto como cadena (string)
+			return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+		})
+		.then(data => {
+			//Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+			console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+		})
+		.catch(error => {
+			//manejo de errores
+			console.log(error);
+		});
+     },[todos]);
+
+	
 	return (
 		<div>
 			<input
@@ -22,7 +51,7 @@ const ToDoList = () => {
 			<ul>
 				{todos.map(todo => {
 					console.log(todo);
-					return <li key={todo.id}>{todo.title}</li>;
+					return <li key={todo.id}>{todo.label}</li>;
 				})}
 			</ul>
 		</div>
